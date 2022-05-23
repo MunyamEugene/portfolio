f1d76904e0db226233e2c5b67a59797805fdd686
MunyamEugene <munyampundueugene3@gmail.com>
2022-05-23 11:58:04 +0200

    initial commit

--- app.js
+++
@@ -1,7 +0,0 @@
-
-const navIcon = document.querySelector('.bugger-menu');
-const navBar = document.querySelector('.nav-bar');
-navIcon.addEventListener('click', () => {
-        navBar.classList.toggle('change');
-    });
-    
