1b60d1a8057098c3a9e19bbc0b608d5a35203194
MunyamEugene <munyampundueugene3@gmail.com>
2022-05-29 13:43:43 +0200

    make form works

--- app.js
+++ app.js
@@ -35,3 +35,37 @@
             navBar.classList.toggle('change');
          container.classList.toggle('left-overlay');
         });
+
+
+    const btn = document.querySelector('form button');
+    const form = document.querySelectorAll('form');
+    const thanku= document.querySelector('.thank-you');
+
+    btn.addEventListener('click',(e)=>{
+        e.preventDefault();
+        const name = form[0][0].value;
+        const email = form[0][1].value;
+        const phone = form[0][2].value;
+         const message = form[0][3].value;
+    
+  
+         
+fetch("https://formspree.io/f/xnqwwggn", {
+	method: "POST",
+	body: JSON.stringify({
+		name,
+		email,
+		phone,
+        message
+
+	}),
+	headers: {
+		"Content-type": "application/json; charset=UTF-8"
+	}
+}).then(()=>{
+    let txt = document.createTextNode('Thank you ! your message has been sent');
+    thanku.appendChild(txt);
+    form[0].reset();
+});
+
+})
